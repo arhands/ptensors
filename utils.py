6968c8cc40cf9fe0efcc2422f501db9af31724be
Andrew Hands <andrewrhands@gmail.com>
2023-09-06 23:06:23 -0500

    test

--- utils.py
+++ utils.py
@@ -4,14 +4,17 @@ from torch import Tensor
 from typing import Literal, TypeAlias, Union, List, Tuple, Dict
 import os
 
-def get_run_path(base_dir: str = 'logs') -> str:
+def get_run_path(base_dir: str = 'logs', use_last_path: bool) -> str:
     if not os.path.exists(base_dir):
         os.mkdir(base_dir)
         run_id = 0
     else:
         contents = os.listdir(base_dir)
         contents = [int(c) for c in contents]
-        run_id = 1 + max(*contents) if len(contents) > 1 else 1 + contents[0]
+        run_id = max(*contents) if len(contents) > 1 else 1 + contents[0]
+        if not use_last_path:
+            run_id += 1
     run_path = f"{base_dir}/{run_id}/"
-    os.mkdir(run_path)
+    if not use_last_path:
+        os.mkdir(run_path)
     return run_path
