c187451c3ad9a5f0ad30087d2f044b87cfc58bdf
Andrew Hands <andrewrhands@gmail.com>
2023-09-07 04:31:21 -0500

    test

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ args = parser.parse_args()
 ds_path = 'data/ZINC'
 if args.run_path is None:
     run_path = get_run_path('runs')
-    
+else:
+    run_path = args.run_path
 
 
 overview_log_path = f"{run_path}/summary.log"
