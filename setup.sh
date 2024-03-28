3de115da6dcd34f6fad0f67cacf77f56d720837e
Andrew Hands <andrewrhands@gmail.com>
2024-03-28 06:00:21 -0500

    added missing packages to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # it is expected that cuda will be available
-conda create -y -n ptensors python=3.10 pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
+conda create -y -n ptensors python=3.10 pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 pyg pytorch-scatter wandb ogb lightning -c pytorch -c nvidia -c pyg -c conda-forge
 conda run -n ptensors pip install pytorch-optimizer
