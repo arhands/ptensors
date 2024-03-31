3b2192ade397b45de92f92854588a1d1805a53b6
Andrew Hands <andrewrhands@gmail.com>
2024-03-31 18:19:05 -0500

    checking for non-integer path names

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def get_run_path(base_dir: str = 'logs') -> str:
         run_id = 0
     else:
         contents = os.listdir(base_dir)
-        contents = [int(c) for c in contents]
+        contents = [int(c) for c in contents if c.isdigit()]
         run_id = max(*contents) if len(contents) > 1 else 1 + contents[0]
         run_id += 1
     run_path = f"{base_dir}/{run_id}/"
